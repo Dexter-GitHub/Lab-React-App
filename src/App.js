027f7517e17eddeefae4b13459c567fc2c67fea4
Dexter <mystyle1057@gmail.com>
2023-10-04 17:10:43 +0900

    [feat] Session6 update 구현: form finish

--- src/App.js
+++ src/App.js
@@ -1,81 +1,126 @@
 
 import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
+import UpdateContent from "./components/UpdateContent";
+import Subject from './components/Subject';
+import Control from './components/Control';
 import './App.css';
-
-class Subject extends Component {
-    render() {
-        return (
-            <header>
-                <h1>WEB</h1>
-                world wide web!
-            </header>
-        );
-    }
-}
-
-class TOC extends Component {
-    render() {
-        return (            
-            <nav>
-                <ul>
-                    <li><a href="1.html">HTML</a></li>
-                    <li><a href="2.html">CSS</a></li>
-                    <li><a href="3.html">JavaScript</a></li>
-                </ul>
-            </nav>
-        );
-    }
-}
-
-class Content extends Component {
-    render() {
-        return (
-            <article>
-                <h2>HTML</h2>
-                HTML is HyperText Markup Language.
-            </article>
-        );
-    }
-}
 
 /* CLASS 기반 App */
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.max_content_id = 3;
+        this.state = {
+            mode: 'read',
+            selected_content_id: 2,
+            subject: {title:"WEB", sub:"world wide web!"},
+            welcome: {title:'Welcome', desc:'Hello, React!!'},
+            contents:[
+                {id:1, title:'HTML', desc:'HTML is for information'},
+                {id:2, title:'CSS', desc:'CSS is for design'},
+                {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+            ]
+        }
+    }
+
+    getReadContent() {
+        var i = 0;
+        while (i < this.state.contents.length) {
+            var data = this.state.contents[i];
+
+            if (data.id === this.state.selected_content_id) {
+                return data;                
+            }
+            i = i + 1;
+        }
+    }
+
+    getContent() {
+        var _title, _desc, _article = null;
+
+        if (this.state.mode === 'welcome') {
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+            _article = <ReadContent title = {_title} desc = {_desc}></ReadContent>;
+        }
+        else if (this.state.mode === 'read') {
+            var _content = this.getReadContent();
+            _article = <ReadContent title = {_content.title} desc = {_content.desc}></ReadContent>;
+        }
+        else if (this.state.mode === 'create') {
+            _article = <CreateContent onSubmit={function(_title, _desc) {
+                // add content to this.state.contents
+                this.max_content_id = this.max_content_id + 1;
+                // NOTE: 원본에 추가하는 코드 - bad
+                /* this.state.contents.push(
+                    {id:this.max_content_id, title:_title, desc:_desc}
+                ); */
+                // NOTE: 복사본을 사용한 코드
+                /* var _contents = this.state.contents.concat(
+                    {id:this.max_content_id, title:_title, desc:_desc}
+                ) */
+                var newContents = Array.from(this.state.contents);
+                newContents.push({id:this.max_content_id, title:_title, desc:_desc});
+                this.setState({
+                    contents:newContents
+                });
+                console.log(_title, _desc);
+            }.bind(this)}></CreateContent>;
+        }
+        else if (this.state.mode === 'update') {
+            _content = this.getReadContent();
+            _article = <UpdateContent data={_content} onSubmit={function(_title, _desc) {
+                // add content to this.state.contents
+                this.max_content_id = this.max_content_id + 1;                
+                /* this.state.contents.push(
+                    {id:this.max_content_id, title:_title, desc:_desc}
+                ); */                
+                var _contents = this.state.contents.concat(
+                    {id:this.max_content_id, title:_title, desc:_desc}
+                )
+                
+                this.setState({
+                    contents:_contents
+                });
+                console.log(_title, _desc);
+            }.bind(this)}></UpdateContent>;
+        }
+
+        return _article;
+    }
+
     render() {
+        console.log('App render');       
         return (
             <div className="App">
-                <Subject></Subject>
-                <TOC></TOC>
-                <Content></Content>
+                <Subject 
+                    title = {this.state.subject.title}
+                    sub = {this.state.subject.sub}
+                    onChangePage = {function() {
+                        this.setState({mode: 'welcome'});
+                    }.bind(this)}>                        
+                </Subject>                
+                <TOC 
+                    onChangePage = {function(id) {                        
+                        this.setState({
+                            mode:'read',
+                            selected_content_id: Number(id)
+                        });
+                    }.bind(this)}
+                    data = {this.state.contents}>                        
+                </TOC>
+                <Control onChangeMode={function(_mode) {
+                    this.setState({
+                        mode:_mode
+                    });
+                }.bind(this)}></Control>
+                {this.getContent()}
             </div>
         )
     }
 }
 
-// NOTE: FUNC 기반 App (react default app) 
-/* 
-import logo from './logo.svg';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
-
 export default App;
-
